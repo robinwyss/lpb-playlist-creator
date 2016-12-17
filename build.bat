37098715cc68a624df27547f96be9c02fa9682bf
Robin Wyss <rowyss@gmail.com>
2016-12-17 13:06:56 -0500

    improved build script to download paket automatically

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
 @echo off
 cls
 
+IF NOT EXIST .paket\paket.exe .paket\paket.bootstrapper.exe
 
 .paket\paket.exe restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-.paket\paket.exe generate-include-script
+.paket\paket.exe generate-include-scripts
 if errorlevel 1 (
   exit /b %errorlevel%
 )

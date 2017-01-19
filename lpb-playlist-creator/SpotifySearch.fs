fb887ec2efed9ce43f21ad93ea00ceec82e786f8
Robin Wyss <robin@configit.com>
2017-01-19 13:29:53 -0500

    parsing json

--- lpb-playlist-creator/SpotifySearch.fs
+++ lpb-playlist-creator/SpotifySearch.fs
@@ -12,8 +12,26 @@ let search artist track =
         //|> JsonValue.Parse
     match result with 
         | JsonValue.Record r -> 
-            let (name, value) = r.[0]
-            name
-        | _ -> "Error"
+            let tracks = r |> Seq.find (fun e -> (fst e) = "tracks") |> snd 
+            let items = tracks.TryGetProperty "items"
+            match items with
+                | Some  i -> 
+                    let first = i.AsArray() |> Seq.take 1 // (fun e -> e.As)
+                    i.ToString()
+                    //first |> Seq.find (fun e -> match e.TryGetProperty "name" with
+                    //                                | Some name -> name.AsString()
+                    //                                | None -> false )
+                | None -> "Error 2"
+            //match tracks with
+            //    | (a,b) -> 
+            //        let items = r |> Seq.find (fun e -> (fst e) = "items")
+            //        match items with 
+            //        | (_, JsonValue.Array a) ->  
+            //            let firstTrack = a |> Seq.take 1
+            //            let e1 = firstTrack |> Seq.take 1 // Seq.find (fun e -> )
+            //            e1.ToString()
+            //        | _ -> "Error 1 " + tracks.ToString()
+            //    | _ -> "Error 1 " + tracks.ToString()
+        | _ -> "Error 1"
 
 
